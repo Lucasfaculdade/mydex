bf55d81b47cc7a2f0adffb49c277a9a56607ab2d
Lucas no DER <3305711T@der.df.gov.br>
2022-11-03 14:05:38 -0300

    atualização dia 02/11

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent implements OnInit {
+  
+  public getAllPokemons: any;
 
-  constructor() { }
+  constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      (res: any) => {
+        this.getAllPokemons= res.results;
+      }
+    );
   }
 
 }
